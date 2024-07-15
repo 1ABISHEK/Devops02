d8384d3a6bcb172d2a1a549378804e2403598870
Abishek K S <159702418+1ABISHEK@users.noreply.github.com>
2024-07-15 15:36:16 +0530

    Update FirstLetter.java

--- FirstLetter.java
+++ FirstLetter.java
@@ -1,8 +1,10 @@
+import java.util.*;
 public class FirstLetter {
 
     
     public static void main(String[] args) {
-        String s = " hi am abishek";
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
         char c[] = s.toCharArray();  
         for(int i=0;i<c.length;i++){
             if(c[i]==' ')
