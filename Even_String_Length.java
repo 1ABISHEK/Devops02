63132d52a0b220fe583a0fdc9e3e27054a780d82
Abishek K S <159702418+1ABISHEK@users.noreply.github.com>
2024-07-15 15:33:38 +0530

    Update Even_String_Length.java

--- Even_String_Length.java
+++ Even_String_Length.java
@@ -1,5 +1,7 @@
 class Even_String_Length{
   public static void main(String args[]){
+    Scanner s = new Scanner(System.in);
+    String sc = s.nextLine();
 for(String w : s.split(" ")){
             if(w.length()%2==0){
                 System.out.println(w+" "+w.length());
