cd292b4ab0fd96d8ba2a40c1ac2bedeb15fc351b
Abishek K S <159702418+1ABISHEK@users.noreply.github.com>
2024-07-15 15:35:27 +0530

    Update square_pattern.java

--- square_pattern.java
+++ square_pattern.java
@@ -1,5 +1,8 @@
+import java.util.*;
 class square_pattern{
 public static void main(String args[]){
+            Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
 for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(i==0||j==0||j==n-1||i==n-1)
