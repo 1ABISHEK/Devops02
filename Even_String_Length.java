73c45fc78319207668a451841e2072f8832ff230
Abishek K S <159702418+1ABISHEK@users.noreply.github.com>
2024-07-15 15:28:08 +0530

    Create Even_String_Length.java

---
+++ Even_String_Length.java
@@ -0,0 +1,9 @@
+class Even_String_Length{
+  public static void main(String args[]){
+for(String w : s.split(" ")){
+            if(w.length()%2==0){
+                System.out.println(w+" "+w.length());
+            }
+        }
+}
+}
